20ba3f9332178b7eedb3afaa2cabedd72db8bb46
Tharushi07 <tharushi.synotec@gmail.com>
2020-03-12 15:58:04 +0530

    #12 Add title 'My list' for index file

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>My list</title>
     </head>
     <body>
            <h1>My List</h1>
