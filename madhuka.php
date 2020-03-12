0a05e1a285f7cd420833aa1a81abc70b976ccd69
Madhuka96 <madhuka.synotec@gmail.com>
2020-03-12 15:16:54 +0530

    #3 Add madhuka.php File

---
+++ madhuka.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Introduction</h1>
+        <h3>Name : Hashini</h3>
+        <h3>Name : Hashini</h3>
+        <h3>Name : Hashini</h3>
+    </body>
+</html>
